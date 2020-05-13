67150313beecc7d67bc021466bf8f2030f8800bb
matyX6 <matyX6@gmail.com>
2020-05-13 09:56:27 +0200

    menu dialog code setup

---
+++ Assets/Scripts/General/MenuDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuDialog : MonoBehaviour
+{
+    [SerializeField] private Text _titleDialog = null;
+    [SerializeField] private Text _textButtonConfirm = null;
+    [SerializeField] private Text _textButtonDecline = null;
+    private Action _actionConfirm = null;
+
+
+    public void ShowDialog(string title, string textButtonConfirm, string textButtonDecline, Action actionConfirm)
+    {
+        _titleDialog.text = title;
+        _textButtonConfirm.text = textButtonConfirm;
+        _textButtonDecline.text = textButtonDecline;
+        _actionConfirm = actionConfirm;
+
+        gameObject.SetActive(true);
+    }
+
+    public void ConfirmAction()
+    {
+        _actionConfirm.Invoke();
+    }
+
+    public void DeclineAction()
+    {
+    }
+}
---
+++ Assets/Scripts/General/MenuDialogContainer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MenuDialogContainer : MonoBehaviour
+{
+    public static MenuDialog Instance { get; private set; }
+
+
+    [SerializeField] private MenuDialog _instance = null;
+
+
+    private void Awake()
+    {
+        Instance = _instance;
+    }
+}
