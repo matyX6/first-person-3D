614ab535d062b0009e516480b0d92520e408de6e
matyX6 <matyX6@gmail.com>
2020-05-13 13:55:58 +0200

    terrain and ground check

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private CharacterController _controller;
+    [SerializeField] private CharacterController _controller = null;
+    [SerializeField] private Transform _groundCheck = null;
+    [SerializeField] private float _groundDistance = .4f;
+    [SerializeField] private LayerMask _groundMask = ~0;
     [SerializeField] private float _speed = 12f;
     [SerializeField] private float _gravity = -9.81f;
     private Vector3 _velocity;
+    bool isGrounded;
+
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+        if (isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
