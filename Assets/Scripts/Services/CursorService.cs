a666791764072c5e82939cd4e3499e9d2eaab5bf
matyX6 <matyX6@gmail.com>
2020-05-13 16:16:05 +0200

    cursor service

---
+++ Assets/Scripts/Services/CursorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class CursorService
+{
+    public static void HideAndLock()
+    {
+        SetCursorStates(false, CursorLockMode.Locked);
+    }
+
+    public static void ShowAndUnlock()
+    {
+        SetCursorStates(true, CursorLockMode.Confined);
+    }
+
+    private static void SetCursorStates(bool visible, CursorLockMode cursorLockMode)
+    {
+        Cursor.visible = visible;
+        Cursor.lockState = cursorLockMode;
+    }
+}
