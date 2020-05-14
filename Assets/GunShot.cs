ef5614417a27a8bc4af8e9dfe4867969ce0b5188
matyX6 <matyX6@gmail.com>
2020-05-14 11:16:56 +0200

    gun shoot with particle on shot

---
+++ Assets/GunShot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GunShot : MonoBehaviour
+{
+    private const string FireAxesName = "Fire1";
+
+
+    [SerializeField] private float _damage = 10f;
+    [SerializeField] private float _range = 100f;
+    [SerializeField] private Camera _fpsCamera = null;
+    [SerializeField] private ParticleSystem _shotFlash = null;
+
+    private void Update()
+    {
+        HandleGunShotInput();
+    }
+
+    private void HandleGunShotInput()
+    {
+        if (Input.GetButtonDown(FireAxesName))
+        {
+            _shotFlash.Play(); 
+
+            RaycastHit hit;
+            if (Physics.Raycast(_fpsCamera.transform.position, _fpsCamera.transform.forward, out hit, _range))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
