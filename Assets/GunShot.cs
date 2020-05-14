7b8429a6eac3ff29575cfda136abeb6d7996722a
matyX6 <matyX6@gmail.com>
2020-05-14 12:33:03 +0200

    gun shots improved

--- Assets/GunShot.cs
+++ Assets/GunShot.cs
@@ -7,9 +7,11 @@ public class GunShot : MonoBehaviour
 
     [SerializeField] private float _damage = 10f;
     [SerializeField] private float _range = 100f;
+    [SerializeField] private float _fireRate = 15f;
     [SerializeField] private Camera _fpsCamera = null;
     [SerializeField] private ParticleSystem _muzzleFlash = null;
     [SerializeField] private GameObject _impactEffect = null;
+    private float _nextTimeToFire = 0f;
 
 
     private void Update()
@@ -19,18 +21,24 @@ public class GunShot : MonoBehaviour
 
     private void HandleGunShotInput()
     {
-        if (Input.GetButtonDown(FireAxesName))
+        if (Input.GetButton(FireAxesName) && Time.time >= _nextTimeToFire)
         {
-            _muzzleFlash.Play(); 
-
-            RaycastHit hit;
-            if (Physics.Raycast(_fpsCamera.transform.position, _fpsCamera.transform.forward, out hit, _range))
-            {
-                Debug.Log(hit.transform.name);
-            }
-
-            GameObject impactEffect = Instantiate(_impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactEffect, 2f);
+            _nextTimeToFire = Time.time + (1f / _fireRate);
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        _muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(_fpsCamera.transform.position, _fpsCamera.transform.forward, out hit, _range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        GameObject impactEffect = Instantiate(_impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactEffect, 2f);
     }
 }
