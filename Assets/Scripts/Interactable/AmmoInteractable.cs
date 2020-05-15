abf00224dcacb62cdd7694e4f2f40d5e7fd2d7b7
matyX6 <matyX6@gmail.com>
2020-05-15 10:54:19 +0200

    purple sphere and ammo interactables added

---
+++ Assets/Scripts/Interactable/AmmoInteractable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class AmmoInteractable : AbstractInteractable
+{
+    public override void Interact()
+    {
+        Debug.Log("Interact with ammo.");
+    }
+}
---
+++ Assets/Scripts/Interactable/PurpleSphereInteractable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PurpleSphereInteractable : AbstractInteractable
+{
+    public override void Interact()
+    {
+        Debug.Log("Interact with purple sphere.");
+    }
+}
