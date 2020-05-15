9e16cc47181ed31cc4011f29668c0aea09a0463a
matyX6 <matyX6@gmail.com>
2020-05-15 15:52:19 +0200

    target animation on hit taken

--- Assets/Scripts/Damageable/Target.cs
+++ Assets/Scripts/Damageable/Target.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class Target : MonoBehaviour, IDamageable
 {
     [SerializeField] private float health = 100f;
+    private Vector3 _defaultScale = Vector3.one;
+    private Vector3 _hitScale = Vector3.one;
 
+
+    private void Start()
+    {
+        _defaultScale = transform.localScale;
+        _hitScale = _defaultScale * 1.3f;
+    }
 
     public void TakeDamage(float damageAmount)
     {
@@ -11,10 +20,19 @@ public class Target : MonoBehaviour, IDamageable
 
         if (health <= 0f)
             Die();
+
+        AnimateDamageTaken();
     }
 
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void AnimateDamageTaken()
+    {
+        transform.DOKill(false);
+        transform.localScale = _hitScale;
+        transform.DOScale(_defaultScale, .2f);
     }
 }
