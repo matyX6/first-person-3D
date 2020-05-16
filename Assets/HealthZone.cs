0befe5860531b6789189bc2ebc4ee8e2906d4445
matyX6 <matyX6@gmail.com>
2020-05-16 15:48:49 +0200

    zones implemented

---
+++ Assets/HealthZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthZone : MonoBehaviour
+{
+    [SerializeField] private float _changeAmount = 10f;
+    [SerializeField] private float _changeRateInSeconds = 1f;
+    private Coroutine _changeHealthRoutine = null;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+
+        if (playerHealth != null)
+            _changeHealthRoutine = StartCoroutine(ChangeHealth(playerHealth));
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        StopCoroutine(_changeHealthRoutine);
+    }
+
+    private IEnumerator ChangeHealth(PlayerHealth playerHealth)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_changeRateInSeconds);
+            playerHealth.ChangeHealth(_changeAmount);
+        }
+    }
+}
