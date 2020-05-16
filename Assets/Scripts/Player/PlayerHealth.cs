78a6806dc1668fdc10a291deb16db07b62d7c060
matyX6 <matyX6@gmail.com>
2020-05-16 14:05:41 +0200

    player health script

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth = 100f;
+    [SerializeField] private RectTransform _healthBar = null;
+    private float _currentHealth = 100f;
+    private float _defaultHealthBarScaleX = 1f;
+
+
+    public void ChangeHealth(float changeAmount)
+    {
+        _currentHealth += changeAmount;
+
+        if (_currentHealth <= 0f)
+            Die();
+        else if (_currentHealth >= _maxHealth)
+            _currentHealth = _maxHealth;
+
+        UpdateHealthBar();
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+        _defaultHealthBarScaleX = _healthBar.localScale.x;
+    }
+
+    private void UpdateHealthBar()
+    {
+        float healthPercentage = _currentHealth / _maxHealth;
+
+        _healthBar.localScale = new Vector2(
+            _defaultHealthBarScaleX * healthPercentage, 
+            _healthBar.localScale.y);
+    }
+
+    private void Die()
+    {
+        Debug.Log("You Died!");
+    }
+}
