ffef2d83e004f0d41a780f1a3073f0bdbcb00238
matyX6 <matyX6@gmail.com>
2020-05-18 12:43:08 +0200

    waypoint patrol lines visualization

--- Assets/Scripts/Enemy/PatrolPath.cs
+++ Assets/Scripts/Enemy/PatrolPath.cs
@@ -9,7 +9,23 @@ public class PatrolPath : MonoBehaviour
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            Gizmos.DrawSphere(transform.GetChild(i).position, WaypointGizmoRadius);
+            int j = GetNextIndex(i);
+            Gizmos.DrawSphere(GetWaypoint(i), WaypointGizmoRadius);
+            Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
         }
     }
+
+    private int GetNextIndex(int i)
+    {
+        if (i + 1 == transform.childCount)
+            return 0;
+
+        return i + 1;
+    }
+
+    private Vector3 GetWaypoint(int i)
+    {
+        return transform.GetChild(i).position;
+    }
 }
+
