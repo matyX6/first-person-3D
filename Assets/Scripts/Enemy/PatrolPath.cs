7c358444ee3638a3d07db55633b03c6dfc431d7e
matyX6 <matyX6@gmail.com>
2020-05-18 11:30:56 +0200

    enemy scripts, patrol path added

---
+++ Assets/Scripts/Enemy/PatrolPath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+    private const float WaypointGizmoRadius = 0.4f;
+
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Gizmos.DrawSphere(transform.GetChild(i).position, WaypointGizmoRadius);
+        }
+    }
+}
