2ef3351f85963338560c6d77906ab52eedd2ddac
matyX6 <matyX6@gmail.com>
2020-05-19 18:13:43 +0200

    music player

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -2,8 +2,16 @@
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private AudioSource _musicPlayer = null;
+
+
     private void ShowMainMenuDialog() //triggered by animator, animation event
     {
         MenuDialogService.ShowMainMenuDialog();
+    }
+
+    private void PlayMusic() //triggered by animator, animation event
+    {
+        _musicPlayer.Play();
     }
 }
